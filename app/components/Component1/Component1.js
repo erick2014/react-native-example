ed2ef33f5f5c6b56881e14770c4d48bc52fc5c4b
erick <erickarmandogarcia@hotmail.com>
2017-06-13 15:41:43 -0500

    Add styling examples

--- app/components/Component1/Component1.js
+++ app/components/Component1/Component1.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {AppRegistry,Text,View} from 'react-native';
+import {AppRegistry,Text,View,StyleSheet} from 'react-native';
 
 export default class Component1 extends Component {
   constructor(props){
@@ -12,13 +12,30 @@ export default class Component1 extends Component {
   }
   render(){
     let name= this.state.showName ? this.state.name: 'No name';
+    console.log("my props.",this.props)
     return(
       <View>
-        <Text>{this.state.message}</Text>
-        <Text>{name}</Text>
+        <Text style={styles.myView}>{this.state.message} </Text>
+        <View style={styles.container}>
+          <View style={styles.box}>
+            <Text>View 1</Text>
+          </View>
+          <View style={styles.box2}>
+            <Text>View 2</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles= StyleSheet.create({
+  myView:{ "backgroundColor":"blue",color:"white",padding:10 },
+  myText:{ "color":"white" },
+  container:{ display:"flex",height:100,flexDirection:"row" },
+  box:{ backgroundColor:"green",flex:1,alignItems:"center", justifyContent:"center" },
+  box2:{ backgroundColor:"yellow",flex:1,alignItems:"center", justifyContent:"center" }
+  
+})
 
 AppRegistry.registerComponent('Component1', () => Component1);
