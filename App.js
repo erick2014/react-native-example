44e5558e006154848128dcd589b733bf74ebd6a8
erick <erickarmandogarcia@hotmail.com>
2017-06-16 19:23:54 -0500

    Add apolloClient connection, add a fetching query and a component to render with that data

--- App.js
+++ App.js
@@ -1,25 +1,58 @@
 /*React stuff*/
-import React from 'react';
-/*React native stuff*/
-import {
-  AppRegistry,
-  Text,
-} from 'react-native';
+import React, { Component } from 'react';
+/*React apollo stuff*/
+import { ApolloClient, createNetworkInterface,ApolloProvider,gql,graphql } from 'react-apollo';
 
+/*React native stuff*/
+import {AppRegistry,Text,View } from 'react-native';
 /*Navigation stuff*/
 import { StackNavigator } from 'react-navigation';
+
 
 /*Custom components*/
 import GeneralView from './app/components/GeneralView/GeneralView';
 import UsersDetails from './app/components/UsersDetails/UsersDetails';
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+
+/*Apollo client setup*/
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://45.56.118.22:4000/graphql',
+  }),
+});
+
+const FeedWithData = graphql(gql`
+  query {
+    getUsers{
+      firstName
+      lastName
+      id
+      email
+    }
+  }`, { options: { notifyOnNetworkStatusChange: true } })(Feed);
+
+function Feed({data}){
+  const { getUsers }=data; 
+  let myText="";
+
+  if( getUsers instanceof Array && getUsers.length>0 ){
+    myText=getUsers[0]["firstName"];
+  }
+  
+  return(
+    <View>
+      <Text>Hello {myText}</Text>
+    </View>
+  )
+}
+
+class HomeScreen extends Component {
+  static navigationOptions = { title: 'Welcome'};
   render() {
     return(
-      <GeneralView message="hey there llaa" {...this.props} />
+      <ApolloProvider client={apolloClient}>
+        <FeedWithData />
+      </ApolloProvider>
     )
   }
 }
